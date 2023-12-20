0f0506e741be2e62c14e249eccf29f3bbb930b46
pPlanello <pplanellodev@gmail.com>
2023-12-20 14:21:30 +0100

    Fix interface to class to pass tests

--- src/main/java/com/pplanello/learning/spring/project/rest/UsersService.java
+++ src/main/java/com/pplanello/learning/spring/project/rest/UsersService.java
@@ -1,11 +1,15 @@
 package com.pplanello.learning.spring.project.rest;
 
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Map;
 
-public interface UsersService {
+@Service
+public class UsersService {
 
-    Mono<List<Map<String, Object>>> getAllUsers();
+    public Mono<List<Map<String, Object>>> getAllUsers() {
+        return null;
+    }
 }
